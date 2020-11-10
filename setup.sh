4edd64e4f8b844ad7904d6c7969c2120596ad0b7
anmakon <annakonrad96@gmail.com>
2020-11-10 09:22:51 +0000

    Added static path

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-read -p "Path of the directory where datasets are stored and read: " dir
+dir="/data"
 echo "DATA_PATH = '$dir'" >> ./superpoint/settings.py
 
-read -p "Path of the directory where experiments data (logs, checkpoints, configs) are written: " dir
+dir="/results"
 echo "EXPER_PATH = '$dir'" >> ./superpoint/settings.py
